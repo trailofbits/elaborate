97138ad0fe08756039c53ada39d3be0e5a9d8662
Samuel Moelius <sam@moeli.us>
2024-11-22 19:57:25 -0500

    Enable Rust feature when corresponding Cargo feature is enabled

--- elaborate/src/lib.rs
+++ elaborate/src/lib.rs
@@ -1,3 +1,69 @@
+#![cfg_attr(dylint_lib = "general", allow(crate_wide_allow))]
+#![allow(stable_features)]
+//
+// Unstable features
+#![cfg_attr(feature = "anonymous_pipe", feature(anonymous_pipe))]
+#![cfg_attr(feature = "buf_read_has_data_left", feature(buf_read_has_data_left))]
+#![cfg_attr(feature = "bufread_skip_until", feature(bufread_skip_until))]
+#![cfg_attr(feature = "core_io_borrowed_buf", feature(core_io_borrowed_buf))]
+#![cfg_attr(feature = "exit_status_error", feature(exit_status_error))]
+#![cfg_attr(feature = "file_buffered", feature(file_buffered))]
+#![cfg_attr(feature = "panic_backtrace_config", feature(panic_backtrace_config))]
+#![cfg_attr(feature = "path_file_prefix", feature(path_file_prefix))]
+#![cfg_attr(feature = "raw_os_error_ty", feature(raw_os_error_ty))]
+#![cfg_attr(feature = "read_buf", feature(read_buf))]
+#![cfg_attr(feature = "seek_stream_len", feature(seek_stream_len))]
+#![cfg_attr(feature = "tcp_linger", feature(tcp_linger))]
+#![cfg_attr(feature = "thread_spawn_unchecked", feature(thread_spawn_unchecked))]
+#![cfg_attr(feature = "write_all_vectored", feature(write_all_vectored))]
+//
+// Linux-specific unstable features
+#![cfg_attr(
+    all(target_os = "linux", feature = "linux_pidfd"),
+    feature(linux_pidfd)
+)]
+#![cfg_attr(
+    all(target_os = "linux", feature = "tcp_deferaccept"),
+    feature(tcp_deferaccept)
+)]
+#![cfg_attr(
+    all(target_os = "linux", feature = "tcp_quickack"),
+    feature(tcp_quickack)
+)]
+#![cfg_attr(
+    all(target_os = "linux", feature = "unix_set_mark"),
+    feature(unix_set_mark)
+)]
+#![cfg_attr(
+    all(target_os = "linux", feature = "unix_socket_ancillary_data"),
+    feature(unix_socket_ancillary_data)
+)]
+//
+// Unix-specific unstable features
+#![cfg_attr(
+    all(unix, feature = "peer_credentials_unix_socket"),
+    feature(peer_credentials_unix_socket)
+)]
+#![cfg_attr(
+    all(unix, feature = "unix_file_vectored_at"),
+    feature(unix_file_vectored_at)
+)]
+#![cfg_attr(all(unix, feature = "unix_socket_peek"), feature(unix_socket_peek))]
+//
+// Windows-specific unstable features
+#![cfg_attr(all(windows, feature = "junction_point"), feature(junction_point))]
+#![cfg_attr(
+    all(windows, feature = "windows_by_handle"),
+    feature(windows_by_handle)
+)]
+#![cfg_attr(
+    all(windows, feature = "windows_change_time"),
+    feature(windows_change_time)
+)]
+//
+// WASI-specific unstable features
+#![cfg_attr(all(target_os = "wasi", feature = "wasi_ext"), feature(wasi_ext))]
+
 use ::std::{any::type_name, fmt::Debug};
 
 #[expect(deprecated, clippy::module_name_repetitions)]
