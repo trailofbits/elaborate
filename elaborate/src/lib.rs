f585b7006c061b62194424173babfcef56199eb0
Samuel Moelius <samuel.moelius@trailofbits.com>
2025-04-28 21:31:16 +0000

    Allow `nonexistent_path_in_comment` in generated code

--- elaborate/src/lib.rs
+++ elaborate/src/lib.rs
@@ -1,4 +1,5 @@
 #![cfg_attr(dylint_lib = "general", allow(crate_wide_allow))]
+#![cfg_attr(dylint_lib = "supplementary", allow(nonexistent_path_in_comment))]
 #![allow(stable_features)]
 //
 // Unstable features
